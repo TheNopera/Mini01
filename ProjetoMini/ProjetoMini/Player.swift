b2f19dca08d671f06a50ade3c0bc77eb0d35a6cd
Daniel Ishida <ishidadecol@gmail.com>
2023-07-11 17:34:05 -0300

    Tested different speed and player size

--- ProjetoMini/ProjetoMini/Player.swift
+++ ProjetoMini/ProjetoMini/Player.swift
@@ -11,20 +11,20 @@ import SpriteKit
 var player:SKSpriteNode = {
     var Player = SKSpriteNode()
     Player.color = .green
-    Player.size = CGSize(width: 50, height: 50)
+    Player.size = CGSize(width: 32, height: 32)
     Player.position = CGPoint(x: 300, y: 100)
     Player.physicsBody?.categoryBitMask = physicsCategory.player.rawValue
     return Player
 }()
 
 func movePlayer(displacement:Double){
-    let playerSpeed: CGFloat = 0.1 // Adjust the player's speed as needed
+    let playerSpeed: CGFloat = 0.05 // Adjust the player's speed as needed
     
     // Calculate the desired movement distance based on the displacement and player's speed
     let movementDistance = displacement * playerSpeed
     
     //DummyPlayer.physicsBody?.applyImpulse(CGVector(dx: movementDistance, dy: 0))
-    
+    //var speed:CGFloat = displacement > 0 ? 10 : -10
     // Apply the movement to the player's position
     let newPosition = CGPoint(x: player.position.x + movementDistance, y: player.position.y)
  
