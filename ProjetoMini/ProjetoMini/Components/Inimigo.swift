6a8c9de6d325162fccf2e767bd928042d0538a2b
Luca Lacerda <lucagabriel520@gmail.com>
2023-07-19 16:39:56 -0300

    fisica da bala arrumada

--- ProjetoMini/ProjetoMini/Components/Inimigo.swift
+++ ProjetoMini/ProjetoMini/Components/Inimigo.swift
@@ -15,8 +15,8 @@ class Inimigo:SKSpriteNode{
     override init(texture: SKTexture?, color: UIColor, size: CGSize) {
         super.init(texture: texture, color: color, size: size)
         
-        self.size.width = 32
-        self.size.height = 36
+        self.size.height = 64
+        self.size.width = 64
         
         name = "enemy"
         physicsBody = SKPhysicsBody(rectangleOf: self.size)
@@ -26,9 +26,12 @@ class Inimigo:SKSpriteNode{
         physicsBody?.allowsRotation = false
         physicsBody?.affectedByGravity = true
         physicsBody?.isDynamic = true
+        physicsBody?.restitution = 0.0
+        
         let ataque = SKAction.run {
             self.attack()
         }
+        
         self.run(.repeatForever(.sequence([ataque,SKAction.wait(forDuration: 2.0)])))
     }
     
@@ -43,20 +46,22 @@ class Inimigo:SKSpriteNode{
     }
     
     func attack(){
-        let bullet = SKShapeNode(circleOfRadius: 2)
+        let bullet = SKShapeNode(circleOfRadius: 12)
         bullet.name = "enemyBullet"
         bullet.fillColor = SKColor(ciColor: .red)
-        bullet.physicsBody = SKPhysicsBody(circleOfRadius: 2)
+        bullet.physicsBody = SKPhysicsBody(circleOfRadius: 6)
         bullet.physicsBody?.categoryBitMask = physicsCategory.enemyBullet.rawValue
+        bullet.physicsBody?.collisionBitMask = physicsCategory.none.rawValue
         bullet.physicsBody?.contactTestBitMask = physicsCategory.player.rawValue
-        bullet.physicsBody?.collisionBitMask = physicsCategory.player.rawValue
-        bullet.physicsBody?.isDynamic = false
+        bullet.physicsBody?.isDynamic = true
         bullet.physicsBody?.affectedByGravity = false
+        bullet.physicsBody?.restitution = 0.0
         
         let variantion = CGFloat.random(in: 1...10)
         let variantDirection = Int.random(in: 1...2)
         
         if variantDirection == 1{
+            
             let offset = CGPoint(x: target!.position.x, y: target!.position.y + variantion) - bullet.position
             let direction = offset.normalized()
             let shootAmount = direction * 1000
@@ -66,7 +71,6 @@ class Inimigo:SKSpriteNode{
             let done = SKAction.removeFromParent()
             
             self.addChild(bullet)
-            print("\(self.position), \(bullet.position)")
             bullet.run(.sequence([mover,done]))
             
         } else{
@@ -79,7 +83,6 @@ class Inimigo:SKSpriteNode{
             let done = SKAction.removeFromParent()
             
             self.addChild(bullet)
-            print("\(self.position), \(bullet.position)")
             bullet.run(.sequence([mover,done]))
         }
     }
--- ProjetoMini/ProjetoMini/Utility /Gameplay.swift
+++ ProjetoMini/ProjetoMini/Utility /Gameplay.swift
@@ -10,9 +10,10 @@ import SpriteKit
 
 //MARK: Physics categories go here, call this enum to set categories.
 enum physicsCategory:UInt32{
+    case none = 0x0
     case platform = 0x1
     case player = 0x2
-    case playerBullet = 0x4
-    case enemy = 0x3
+    case playerBullet = 0x3
+    case enemy = 0x4
     case enemyBullet = 0x5
 }
