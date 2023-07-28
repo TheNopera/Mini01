44602b4da9e9ccacf7bc3e8dd2d621fe2154ecc6
MateusMPires <mateusmp.df@gmail.com>
2023-07-28 19:02:32 -0300

    Sounds and Music Hover and Resume Button

--- ProjetoMini/ProjetoMini/Components/HUDNode.swift
+++ ProjetoMini/ProjetoMini/Components/HUDNode.swift
@@ -14,14 +14,26 @@ class HUDNode: SKNode {
     // MARK: Menu Properties
     private var menuShape: SKShapeNode!
     private var menuNode: SKSpriteNode!
-    
     private var startNode: SKLabelNode!
+    
+    private var configShape: SKSpriteNode!
+    private var configTitle: SKLabelNode!
+    private var soundLabel: SKLabelNode!
+    private var soundNode: SKSpriteNode!
+    private var musicLabel: SKLabelNode!
+    private var musicNode: SKSpriteNode!
+    private var returnNode: SKSpriteNode!
+    private var returnLabel: SKLabelNode!
+    
+    private var isSoundOn = false
+    private var isMusicOn = false
     
     // MARK: Paused Properties
     private var inGamePauseNode: SKSpriteNode!
     
     private var pauseNodeShape: SKShapeNode!
     private var pauseNode: SKSpriteNode!
+    private var restartNode: SKSpriteNode!
     private var resumeNode: SKSpriteNode!
     private var quitNode: SKSpriteNode!
     private var tituloPause: SKLabelNode!
@@ -67,6 +79,26 @@ class HUDNode: SKNode {
             updateBtn(node: startNode, event: isStart)
         }
     }
+    private var isConfig = false {
+        didSet {
+            updateBtn(node: menuSettings, event: isConfig)
+        }
+    }
+    private var isSound = false {
+        didSet {
+            updateBtn(node: soundNode, event: isSound)
+        }
+    }
+    private var isMusic = false {
+        didSet {
+            updateBtn(node: musicNode, event: isMusic)
+        }
+    }
+    private var isReturn = false {
+        didSet {
+            updateBtn(node: returnNode, event: isReturn)
+        }
+    }
      var isPause = false {
         didSet {
             updateBtn(node: inGamePauseNode, event: isPause)
@@ -80,6 +112,11 @@ class HUDNode: SKNode {
     private var isQuit = false {
         didSet {
             updateBtn(node: quitNode, event: isQuit)
+        }
+    }
+    private var isRestart = false {
+        didSet {
+            updateBtn(node: restartNode, event: isRestart)
         }
     }
     private var IsHome = false {
@@ -119,6 +156,21 @@ class HUDNode: SKNode {
             print("StartButton")
         }
         
+        if node.name == "Configuration" && !isConfig {
+            isConfig = true
+        }
+        
+        if node.name == "Sound" && !isSound {
+            isSound = true
+        }
+        
+        if node.name == "Music" && !isMusic {
+            isMusic = true
+        }
+        
+        if node.name == "Return from Config" && !isReturn {
+            isReturn = true
+        }
         // PAUSED GAME
         if node.name == "Pause" && !isPause {
             isPause = true
@@ -130,6 +182,9 @@ class HUDNode: SKNode {
           
         }
         
+        if node.name == "Restart" && !isRestart {
+            isRestart = true
+        }
         if node.name == "Quit" && !isQuit {
            isQuit = true
         }
@@ -164,10 +219,39 @@ class HUDNode: SKNode {
             }
         }
         
+        if isConfig {
+            setupConfigMenu()
+            startNode.removeFromParent()
+            isConfig = false
+        }
+        
+        if isSound {
+            toggleSound()
+            isSound = false
+        }
+        
+        if isMusic {
+            toggleMusic()
+            isMusic = false
+        }
+        if isReturn {
+            configShape.removeFromParent()
+            configTitle.removeFromParent()
+            soundLabel.removeFromParent()
+            soundNode.removeFromParent()
+            musicLabel.removeFromParent()
+            musicNode.removeFromParent()
+            returnNode.removeFromParent()
+            returnLabel.removeFromParent()
+
+            addChild(startNode)
+            isReturn = false
+        }
         //PAUSED GAME
         if isPause {
             print("Botão Pause foi clicado")
             setupPausePanel()
+            setupResumeNode()
             scene?.isPaused = true
             isPause = false
             gamePaused = true
@@ -178,6 +262,7 @@ class HUDNode: SKNode {
 //            pauseNodeShape.removeFromParent()
             quitNode.removeFromParent()
             tituloPause.removeFromParent()
+            restartNode.removeFromParent()
             resumeNode.removeFromParent()
             //configNode.removeFromParent()
             scene?.isPaused = false
@@ -185,6 +270,15 @@ class HUDNode: SKNode {
             print("is resume recebe false")
             gamePaused = false
             
+        }
+        
+        if isRestart {
+            isRestart = false
+            if let _ = easeGameScene {
+                let scene = GameScene(size: CGSize(width: screenWidth, height: screenHeight))
+                scene.scaleMode = .aspectFill
+                skView.presentScene(scene, transition: .fade(withDuration: 1.5))
+            }
         }
         
         if isQuit {
@@ -229,6 +323,9 @@ class HUDNode: SKNode {
             isStart = startNode.contains(touch.location(in: parent))
         } else
         
+        if let parent = menuSettings.parent {
+            isConfig = menuSettings.contains(touch.location(in: parent))
+        }
 //        if let parent = pauseNode?.parent {
 //            isPause = pauseNode.contains(touch.location(in: parent))
 //        } else
@@ -238,8 +335,8 @@ class HUDNode: SKNode {
          Se o node for diferente do update, cancela a funcao (comparação no touchesmoved)
          
          */
-        if let parent = resumeNode?.parent {
-            isResume = resumeNode.contains(touch.location(in: parent))
+        if let parent = restartNode?.parent {
+            isResume = restartNode.contains(touch.location(in: parent))
         } else
         
         if let parent = quitNode?.parent {
@@ -250,8 +347,8 @@ class HUDNode: SKNode {
             IsHome = homeNode.contains(touch.location(in: parent))
         } else
         
-        if let parent = againNode?.parent {
-            isAgain = againNode.contains(touch.location(in: parent))
+        if let parent = restartNode?.parent {
+            isAgain = restartNode.contains(touch.location(in: parent))
         }
     }
 }
@@ -401,7 +498,7 @@ extension HUDNode {
         
         menuShape = SKShapeNode(rect: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: screenHeight))
         menuShape.strokeColor = SKColor(ciColor: .clear)
-        menuShape.zPosition = 56.0
+        menuShape.zPosition = 53.0
         menuShape.name = "Start"
         addChild(menuShape)
         
@@ -468,6 +565,7 @@ extension HUDNode {
         menuSettings = SKSpriteNode(imageNamed: "menu-settings")
         menuSettings.zPosition = 55.0
         menuSettings.setScale(0.75)
+        menuSettings.name = "Configuration"
         menuSettings.position = CGPoint(x: screenWidth*0.93, y: screenHeight*0.90)
         addChild(menuSettings)
         
@@ -491,6 +589,89 @@ extension HUDNode {
     }
 }
 
+extension HUDNode {
+    
+    // MARK: Configuration Panel
+    func setupConfigMenu() {
+        
+        configShape = SKSpriteNode(imageNamed: "NevoaDoFundo")
+        configShape.zPosition = 60.0
+        configShape.position = CGPoint(x: screenWidth*0.5, y: screenHeight*0.5)
+        addChild(configShape)
+        
+        configTitle = SKLabelNode(text: "Settings")
+        configTitle.zPosition = 60.0
+        configTitle.fontName = "KarmaticArcade"
+        configTitle.position = CGPoint(
+            x: screenWidth*(0.49) + 3,
+            y: screenHeight*(0.65))
+        addChild(configTitle)
+        
+        soundLabel = SKLabelNode(text: "sound effects")
+        soundLabel.zPosition = 60.0
+        soundLabel.fontName = "JupiterCrashBRK"
+        soundLabel.fontSize = 20.0
+        soundLabel.position = CGPoint(x: screenWidth*0.467, y: screenHeight*0.5)
+        addChild(soundLabel)
+        
+        soundNode = isSoundOn ? SKSpriteNode(imageNamed: "sound-on") : SKSpriteNode(imageNamed: "sound-off")
+        soundNode.size = CGSize(width: 28, height: 28)
+        soundNode.name = "Sound"
+        soundNode.zPosition = 60.0
+        soundNode.position = CGPoint(x: screenWidth*0.5 + 50, y: screenHeight*0.52)
+        addChild(soundNode)
+        
+        musicLabel = SKLabelNode(text: "music")
+        musicLabel.zPosition = 60.0
+        musicLabel.fontName = "JupiterCrashBRK"
+        musicLabel.fontSize = 20.0
+        musicLabel.position = CGPoint(x: screenWidth*0.47, y: screenHeight*0.4)
+        addChild(musicLabel)
+        
+        musicNode = isMusicOn ? SKSpriteNode(imageNamed: "music-on") : SKSpriteNode(imageNamed: "music-off")
+        musicNode.zPosition = 60.0
+        musicNode.name = "Music"
+        musicNode.size = CGSize(width: 28, height: 28)
+        musicNode.position = CGPoint(
+            x: screenWidth/2 + 50,
+            y: screenHeight*0.42)
+        addChild(musicNode)
+        
+        returnNode = SKSpriteNode(imageNamed: "Button")
+        returnNode.zPosition = 61.0
+        returnNode.name = "Return from Config"
+        returnNode.position = CGPoint(
+            x: screenWidth*(0.5),
+            y: screenHeight*0.25)
+        addChild(returnNode)
+        
+        returnLabel = SKLabelNode()
+        returnLabel.text = "return"
+        returnLabel.fontSize = 20.0
+        returnLabel.fontName = "JupiterCrashBRK"
+        returnLabel.color = .white
+        returnLabel.zPosition = 61.0
+        returnLabel.position = CGPoint(
+            x: screenWidth*(0.5),
+            y: screenHeight*0.23)
+        addChild(returnLabel)
+
+    }
+    
+    func toggleSound() {
+        isSoundOn = !isSoundOn
+        let soundToggle = childNode(withName: "Sound") as! SKSpriteNode
+        soundToggle.texture = isSoundOn ? SKTexture(imageNamed: "sound-on") : SKTexture(imageNamed: "sound-off")
+    }
+    
+    func toggleMusic() {
+        isMusicOn = !isMusicOn
+        let musicToggle = childNode(withName: "Music") as! SKSpriteNode
+        musicToggle.texture = isMusicOn ? SKTexture(imageNamed: "music-on") : SKTexture(imageNamed: "music-off")
+        
+    }
+}
+
 //MARK: Pause
 extension HUDNode {
     
@@ -508,6 +689,17 @@ extension HUDNode {
 
     }
     
+    func setupResumeNode() {
+        resumeNode = SKSpriteNode(imageNamed: "resume-button")
+        resumeNode.name = "Resume"
+        resumeNode.size = CGSize(width: 32, height: 32)
+        resumeNode.zPosition = 55.0
+        resumeNode.position = CGPoint(
+            x: screenWidth*0.93,
+            y: screenHeight*0.90)
+        addChild(resumeNode)
+        isUserInteractionEnabled = true
+    }
     // MARK: Enter the Paused Panel
     func setupPausePanel() {
 //        pauseNodeShape = SKShapeNode(rect: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: screenHeight))
@@ -532,14 +724,14 @@ extension HUDNode {
         quitNode.name = "Quit"
         addChild(quitNode)
         
-        // MARK: Resume Node
-        resumeNode = SKSpriteNode(imageNamed: "back-button")
-        resumeNode.zPosition = 55.0
-        resumeNode.position = CGPoint(
+        // MARK: Restart Node
+        restartNode = SKSpriteNode(imageNamed: "back-button")
+        restartNode.zPosition = 55.0
+        restartNode.position = CGPoint(
             x: screenWidth*(0.62),
             y: screenHeight*(0.45))
-        resumeNode.name = "Resume"
-        addChild(resumeNode)
+        restartNode.name = "Restart"
+        addChild(restartNode)
         
         tituloPause = SKLabelNode()
         tituloPause.fontSize = 40.0
@@ -552,13 +744,13 @@ extension HUDNode {
             y: screenHeight*(0.65))
         addChild(tituloPause)
         
-        /*configNode = SKSpriteNode(imageNamed: "menu-settings")
-        configNode.zPosition = 55.0
-        configNode.position = CGPoint(
-            x: screenWidth*(0.49),
-            y: screenHeight*(0.45))
-        configNode.name = "Config"
-        addChild(configNode)*/
+//        menuSettings = SKSpriteNode(imageNamed: "menu-settings")
+//        menuSettings.zPosition = 55.0
+//        menuSettings.position = CGPoint(
+//            x: screenWidth*(0.49),
+//            y: screenHeight*(0.45))
+//        menuSettings.name = "Config"
+//        addChild(menuSettings)
     }
 }
 
